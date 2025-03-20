47064d8d576941e71757af96afdbb7bcb64f7aa4
oscarGR-123 <j.oscar.rf.223@gmail.com>
2025-03-20 07:53:07 -0600

    Add files via upload

---
+++ newfile3.cxx
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("la suma de %d + %d es = %d\n",4,8,5+8);
+     printf("la suma de %d + %d es = %d\n",78787,3259,78787+3259);
+    system("pause");
+    return 0;
+    }
